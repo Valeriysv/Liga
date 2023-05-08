eefd8f130e46b747db24c6c817ad0fecc5703ced
Valeriysv <85869324+Valeriysv@users.noreply.github.com>
2023-05-08 23:21:25 +0300

    Update Data.java

--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -2,7 +2,7 @@ public class Data {
     public String getURL = "https://auth.ligazakon.net/";
     public String firstName = "Валерій";
     public String lastName = "Свєтлов";
-    public String email = "pbsvetlov@gmail.com";
+    public String email = "pbsvetlov1989@gmail.com";
     public String phone = "+380636543607";
     public String company = "Svetlov";
     public String password = "q\\FG0l27";
